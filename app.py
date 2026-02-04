e2180a4865fa4098e52edd628e81c82c19c9c588
Santhosh Kumar .E <sansanthosh5621@gmail.com>
2026-02-04 16:50:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 import schedule
 from twilio.rest import Client
 
+
 # PDF
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
@@ -302,5 +303,7 @@ send_pdf()   # manual test PDF WhatsApp send
 # ---------------- RUN ---------------- #
    # manual test WhatsApp send
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
